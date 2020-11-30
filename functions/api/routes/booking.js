d314b5c8714756aa480b60561d59c5cf91dc833a
Cerebro <cerebropvt@gmail.com>
2020-11-30 13:54:02 +0530

    Fourth Commit

--- functions/api/routes/booking.js
+++ functions/api/routes/booking.js
@@ -155,6 +155,40 @@ myrouter.get('/filterDate/:date1',(req,res) => {
     })();
 });
 
+//Filter by Date and Time Slot
+myrouter.get('/filterDateTime/:date1/:startTime/:endTime',(req,res) => {
+    // eslint-disable-next-line consistent-return
+    (async() => {
+        try
+        {
+            let query = db.collection(dbName);  
+            let response = [] ;
+
+            await query.where("date1", '==', req.params.date1)
+            .where("startTime", "==", req.params.startTime)
+            .where("endTime", "==", req.params.endTime)
+            .get().then(snap => {
+                
+                if(snap.empty){
+                    console.log('No such data found.');
+                    return res.status(404).json({data : "no data"})
+                }
+                else{
+                    snap.forEach((doc) => {
+                        response.push(doc.data());
+                    })
+                    console.log('----Read Required Data----');
+                    return res.status(200).json(response);
+                }
+            })
+       }
+        catch(error) 
+        {
+            console.log('Error!');
+            return res.status(500).json(error);
+        }
+    })();
+});
 
 
 
